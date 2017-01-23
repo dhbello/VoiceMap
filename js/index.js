6e4f27c75742765f3fe0b02296c252d67d058d28
David Bello <david.bello@sinesat.com>
2017-01-23 15:32:38 -0500

    Version 0.9

--- js/index.js
+++ js/index.js
@@ -6,13 +6,23 @@ var $$ = Dom7;
 var map;
 var view;
 var mapLayer;
+
 var marker;
+var markerG;
+
 var glPoint;
 var glPointG;
+
+var currentPointX;
+var currentPointY;
 var currentPoint;
 var currentUser;
-var imageCache;
+var currentChats;
+var currentChat;
+
 var registrationData;
+var cachePath;
+var cacheParticipantes;
 
 var modeManual = false;
 var photoURLS = new Array();
@@ -20,6 +30,10 @@ var msgtitle = "VoiceMap";
 var baseMapUrl = "http://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/Mapa_Referencia/Mapa_Base/MapServer";
 
 var _url_registro = 'https://voicemap-153216.appspot.com/Registro?';
+var _url_conversacion = 'https://voicemap-153216.appspot.com/Conversacion?';
+var _url_mensajes = 'https://voicemap-153216.appspot.com/Mensaje?';
+var _url_photo = 'https://dinamica-147714.appspot.com/Imagen';
+var _url_audio = 'https://dinamica-147714.appspot.com/Audio';
 
 gotoLogin();
 
@@ -39,7 +53,6 @@ function onDeviceReady() {
 }
 
 function init() {
-    currentUser = window.localStorage.getItem("user");
     if (isPhoneGapExclusive()) {
         gotoLogin();
         try {
@@ -81,7 +94,7 @@ function init() {
             sendAlert('Esta aplicaci&oacute;n requiere conexi&oacute;n a internet.');
             $("#bienvenida-toolbar").hide();
         }
-    } else {        
+    } else {
         if (window.localStorage.getItem("user") == null) {
             gotoLogin();
         } else {
@@ -113,12 +126,20 @@ function initMap2() {
         slider: false
     });
     dojo.connect(map, "onClick", function (evt) {
+        if (evt.graphic != null) {
+            gotoChat(evt.graphic.attributes.conversacionId);
+        };
         setLocationPoint(evt);
     });
     marker = new esri.symbol.PictureMarkerSymbol();
     marker.setHeight(44);
     marker.setWidth(28);
     marker.setUrl("css/Location_Icon.png");
+
+    markerG = new esri.symbol.PictureMarkerSymbol();
+    markerG.setHeight(44);
+    markerG.setWidth(28);
+    markerG.setUrl("css/Marker_Icon.png");
 
     mapLayer = new esri.layers.ArcGISTiledMapServiceLayer("http://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/Mapa_Referencia/Mapa_Base/MapServer/");
     map.addLayer(mapLayer);
@@ -198,7 +219,7 @@ function hideAll() {
     $("#chat-toolbar").hide();
     $("#settingsDiv").hide();
     $("#settings-toolbar").hide();
-    
+
     myApp.hideToolbar(".toolbar");
 };
 
@@ -207,21 +228,23 @@ function login() {
         window.plugins.googleplus.trySilentLogin({},
                 function (obj) {
                     obj.registrationId = registrationData;
-                    var registroURL = _url_registro + "email=" + encodeURIComponent(obj.email)
-                          + "&userId=" + obj.userId + "&name=" + encodeURIComponent(obj.displayName) + "&photo=" + encodeURIComponent(obj.imageUrl) + "&registrationId=" + encodeURIComponent(obj.registrationId);
+                    currentUser = obj;
+                    var registroURL = _url_registro + "email=" + encodeURIComponent(currentUser.email)
+                      + "&userId=" + currentUser.userId + "&displayName=" + encodeURIComponent(currentUser.displayName) + "&imageUrl=" + encodeURIComponent(currentUser.imageUrl) + "&registrationId=" + encodeURIComponent(currentUser.registrationId);
                     $.ajax({
                         url: registroURL,
                         type: 'GET',
-                        success: function () {
-                           
+                        dataType: 'json',
+                        success: function (response) {
+                            currentChats = response.conversaciones;
+                            hideAll();
+                            gotoMap();
+                            updateUser();
                         },
                         error: function () {
-                           
+                            sendAlert("Error en el inicio de session.");
                         }
-                    });                                     
-                    hideAll();
-                    gotoMap();
-                    updateUser();
+                    });
                 },
                 function (msg) {
                     window.plugins.googleplus.login({
@@ -230,16 +253,21 @@ function login() {
                     },
                     function (obj) {
                         obj.registrationId = registrationData;
-                        var registroURL = _url_registro + "email=" + encodeURIComponent(obj.email)
-                          + "&userId=" + obj.userId + "&name=" + encodeURIComponent(obj.displayName) + "&photo=" + encodeURIComponent(obj.imageUrl) + "&registrationId=" + encodeURIComponent(obj.registrationId);
+                        currentUser = obj;
+                        var registroURL = _url_registro + "email=" + encodeURIComponent(currentUser.email)
+                          + "&userId=" + currentUser.userId + "&displayName=" + encodeURIComponent(currentUser.displayName) + "&imageUrl=" + encodeURIComponent(currentUser.imageUrl) + "&registrationId=" + encodeURIComponent(currentUser.registrationId);
                         $.ajax({
                             url: registroURL,
                             type: 'GET',
-                            success: function () {
-
+                            dataType: 'json',
+                            success: function (response) {
+                                currentChats = response.conversaciones;
+                                hideAll();
+                                gotoMap();
+                                updateUser();
                             },
                             error: function () {
-
+                                sendAlert("Error en el inicio de session.");
                             }
                         });
                         hideAll();
@@ -253,10 +281,24 @@ function login() {
                 }
              );
     } else {
-        hideAll();
-        gotoMap();
-        updateUser();
-    }    
+        currentUser = { userId: 109917423592778214535, email: "dbello@catastrobogota.gov.co", displayName: "David Hernando Bello Ladino", imageUrl: "https://lh4.googleusercontent.com/-yrsxwsfx9jQ/AAAAAAAAAAI/AAAAAAAAAAk/dBZAfoGLEU8/photo.jpg", registrationId: null }
+        var registroURL = _url_registro + "email=" + encodeURIComponent(currentUser.email)
+          + "&userId=" + currentUser.userId + "&displayName=" + encodeURIComponent(currentUser.displayName) + "&imageUrl=" + encodeURIComponent(currentUser.imageUrl) + "&registrationId=" + encodeURIComponent(currentUser.registrationId);
+        $.ajax({
+            url: registroURL,
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                currentChats = response.conversaciones;
+                hideAll();
+                gotoMap();
+                updateUser();
+            },
+            error: function () {
+                sendAlert("Error en el inicio de session.");
+            }
+        });
+    }
 };
 
 function gotoLogin() {
@@ -266,6 +308,7 @@ function gotoLogin() {
 }
 
 function gotoMap() {
+    cachePath = "map";
     hideAll();
     $("#mapDiv").css("left", "0px");
     $("#mapDiv").css("position", "");
@@ -278,40 +321,200 @@ function gotoMap() {
 };
 
 function gotoListado() {
+    cachePath = "listado";
     hideAll();
     $("#listadoDiv").show();
     $("#listado-toolbar").show();
 }
 
+function gotonewChat() {
+    currentChat = null;
+    $("#ftitulo").val("");
+    cacheParticipantes = [];
+    cacheParticipantes.push(currentUser.email);
+    updateParticipantes();
+    hideAll();
+    $("#settingsDiv").show();
+    $("#settings-toolbar").show();
+};
+
 function gotoSettings() {
+    var currentC = null;
+    for (var i = 0; i < currentChats.length; i++) {
+        if (currentChat == currentChats[i].id) {
+            currentC = currentChats[i];
+        };
+    };
+    $("#ftitulo").val(currentC.titulo);
+    $("#fdireccion").val(currentC.direccion);
+    cacheParticipantes = currentC.participantes.slice();
+    updateParticipantes();
     hideAll();
     $("#settingsDiv").show();
     $("#settings-toolbar").show();
 }
 
-function gotoChat() {
+function acceptSettings() {
+    var strParticipantes = "";
+    var strId;
+    if (currentChat == null) {
+        strId = new Date().getTime();
+    } else {
+        strId = currentChat;
+    }
+    for (var i = 0; i < cacheParticipantes.length; i++) {
+        strParticipantes = strParticipantes + "&participante=" + cacheParticipantes[i];
+    };
+    $.ajax({
+        url: _url_conversacion + "cmd=update&email=" + currentUser.email + "&id=" + strId + "&titulo=" + encodeURIComponent($("#ftitulo").val()) + "&direccion=" + encodeURIComponent($("#fdireccion").val()) + "&latitud=" + currentPointX + "&longitud=" + currentPointY + strParticipantes,
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            currentChats = response.conversaciones;
+            updateUser();
+            gotoChat(response.id);
+        },
+        error: function () {
+            sendAlert("Error actualizando/creando conversacion.");
+        }
+    });
+}
+
+function cancelSettings() {
+    if (currentChat == null) {
+        gotoMap();
+    } else {
+        gotoChat(currentChat);
+    };
+}
+
+function gotoBack() {
+    if (cachePath == "map") {
+        gotoMap();
+    } else {
+        gotoListado();
+    };
+};
+
+function gotoChat(id) {
+    currentChat = id;
+    $("#listadoMensajes").html("");
+    $.ajax({
+        url: _url_conversacion + "cmd=get&conversacionId=" + currentChat,
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            for (var i = 0; i < response.mensajes.length; i++) {
+                var strHtml = "<div class='card facebook-card'>";
+                strHtml = strHtml + "<div class='card-header no-border'>";
+
+                var strUser = null;
+                for (var j = 0; j < response.usuarios.length; j++) {
+                    if (response.mensajes[i].email == response.usuarios[j].email) {
+                        strUser = response.usuarios[j];
+                    }
+                }
+
+                if (strUser == null) {
+                    strHtml = strHtml + "<div class='facebook-avatar'><img src='images/Profile.jpg' width='34' height='34'></div>";
+                    strHtml = strHtml + "<div class='facebook-name'>" + response.mensajes[i].email + "</div>";
+                } else {
+                    strHtml = strHtml + "<div class='facebook-avatar'><img src='" + strUser.imageUrl + "' width='34' height='34'></div>";
+                    strHtml = strHtml + "<div class='facebook-name'>" + strUser.displayName + "</div>";
+                }
+
+                strHtml = strHtml + "<div class='facebook-date'>" + response.mensajes[i].timestamp + "</div>";
+                strHtml = strHtml + "</div>";
+                strHtml = strHtml + "<div class='card-content'>";
+                strHtml = strHtml + response.mensajes[i].contenido;
+                strHtml = strHtml + "</div>";
+                strHtml = strHtml + "</div>";
+                $("#listadoMensajes").append(strHtml);
+            }
+            $("#listadoMensajes").append("<br />");
+        },
+        error: function () {
+            sendAlert("Error en el inicio de session.");
+        }
+    });
     hideAll();
     $("#chatDiv").show();
     $("#chat-toolbar").show();
     myApp.showToolbar(".toolbar");
 }
 
-function dial() {
-    if ($("#speed-dial").hasClass('speed-dial-opened')) {
-        $('#speed-dial').removeClass('speed-dial-opened');
-    } else {
-        $('#speed-dial').addClass('speed-dial-opened');
-    }
+function sendText() {
+    var strTxt = $("#txtMsg").val();
+    $("#txtMsg").val("");
+    $.ajax({
+        url: _url_mensajes + "cmd=create&conversacionId=" + currentChat + "&id=" + new Date().getTime() + "&email=" + encodeURIComponent(currentUser.email) + "&contenido=" + strTxt,
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            gotoChat(currentChat);
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function sendPhoto() {
+    myApp.modal({
+        title: 'VoiceMap',
+        text: 'De donde quieres cargar la foto',
+        buttons: [
+          {
+              text: 'Camara',
+              onClick: function () {
+                  addPhotos(Camera.PictureSourceType.CAMERA);
+              }
+          },
+          {
+              text: 'Galeria',
+              onClick: function () {
+                  addPhotos(Camera.PictureSourceType.SAVEDPHOTOALBUM);
+              }
+          }
+        ]
+    });
+}
+
+function sendAudio() {
+    navigator.device.capture.captureAudio(captureAudioSuccess, captureAudioFail, {
+        limit: 1, duration: 30
+    });
+}
+
+function captureAudioSuccess(audioURI) {
+    myApp.showPreloader("Cargando audio, por favor, espere." + JSON.stringify(audioURI));
+
+    var fail, ft, options, params, win;
+    options = new FileUploadOptions();
+    options.fileKey = "nva_audio";
+    options.fileName = "audio_" + new Date().getTime();
+    ft = new FileTransfer();
+    ft.upload(audioURI[0], _url_audio + "cmd=create&conversacionId=" + currentChat + "&id=" + new Date().getTime() + "&email=" + encodeURIComponent(currentUser.email), uploadAudioSuccessFT, uploadAudioFail, options);
+}
+
+function captureAudioFail(audioURI) {
+    sendAlert("Error en la captura del audio");
+}
+
+function uploadAudioSuccessFT(response) {
+    gotoChat(currentChat);
 };
 
-function nuevoParticipante() {
-    myApp.prompt('Correo del nuevo participante?', function (value) {
-        myApp.alert('Nuevo participante "' + value + '" agregado.');
-    });
+function uploadAudioFail(error) {
+    myApp.hidePreloader();
+    setTimeout(function () {
+        sendAlert("No se pudo cargar el audio, por favor, intente m&aacute;s tarde.");
+    }, 1500);
 };
+
 
 function addPhotos(sourceType) {
-    navigator.camera.getPicture(captureSuccess, captureFail, {
+    navigator.camera.getPicture(captureImagenSuccess, captureImagenFail, {
         destinationType: Camera.DestinationType.FILE_URI,
         sourceType: sourceType,
         quality: 50,
@@ -329,38 +532,91 @@ function captureSuccess(imageURI) {
     options.fileKey = "nva_imagen";
     options.fileName = "imagen_" + new Date().getTime() + ".jpg";
     ft = new FileTransfer();
-    imageCache = imageURI;
-    ft.upload(imageURI, _url_photo, uploadSuccessFT, uploadFail, options);
+    ft.upload(imageURI, _url_photo + "cmd=create&conversacionId=" + currentChat + "&id=" + new Date().getTime() + "&email=" + encodeURIComponent(currentUser.email), uploadImagenSuccessFT, uploadImagenFail, options);
 }
 
-function captureFail(imageURI) {
+function captureImagenFail(imageURI) {
     sendAlert("Error en la captura de la imagen");
 }
 
-function uploadSuccessFT(response) {
-    myApp.hidePreloader();
-    setTimeout(function () {
-        sendAlert("Foto cargada exitosamente.");
-    }, 1500);
-    var objResponse;
-    objResponse = response.response;
-    photoURLS.push(objResponse);
-    //$('#photolist').append('<img class="image_thumb" src="' + imageCache + '" />');
+function uploadImagenSuccessFT(response) {
+    gotoChat(currentChat);
 };
 
-function uploadFail(error) {
+function uploadImagenFail(error) {
     myApp.hidePreloader();
     setTimeout(function () {
         sendAlert("No se pudo cargar la foto, por favor, intente m&aacute;s tarde.");
     }, 1500);
 };
 
-function submitMsg() {
+function dial() {
+    if ($("#speed-dial").hasClass('speed-dial-opened')) {
+        $('#speed-dial').removeClass('speed-dial-opened');
+    } else {
+        $('#speed-dial').addClass('speed-dial-opened');
+    }
+};
 
+function nuevoParticipante() {
+    myApp.prompt('Correo del nuevo participante?', 'VoiceMap', function (value) {
+        cacheParticipantes.push(value);
+        updateParticipantes();
+    });
 };
 
 function updateUser() {
+    $("#user_name").html(currentUser.displayName);
+    $("#user_email").html(currentUser.email);
+    $("#listadoConversaciones").html("");
+    for (var i = 0; i < currentChats.length; i++) {
+        var strHtml = "<li>";
+        strHtml = strHtml + "<a href='#' class='item-link item-content' onclick='gotoChat(\"" + currentChats[i].id + "\");'>";
+        //strHtml = strHtml + "<div class='item-media'>";
+        //strHtml = strHtml + "<img src='images/Map.png' style='width: 150px;' />";
+        //strHtml = strHtml + "</div>";
+        strHtml = strHtml + "<div class='item-inner'>";
+        strHtml = strHtml + "<div class='item-title-row'>";
+        strHtml = strHtml + "<div class='item-title'>" + currentChats[i].titulo + "</div>";
+        strHtml = strHtml + "</div>";
+        //strHtml = strHtml + "<div class='item-subtitle'>Persona 1, Persona 2, Persona 3</div>";
+        //strHtml = strHtml + "<div class='item-text'>Ultimo mensaje: Fecha y texto...</div>";        
+        strHtml = strHtml + "</div>";
+        strHtml = strHtml + "</a>";
+        strHtml = strHtml + "</li>";
+        $("#listadoConversaciones").append(strHtml);
 
+
+        glPointG.clear();
+        try {
+            var currentPoint = new esri.geometry.Point(currentChats[i].latitud, currentChats[i].longitud, { wkid: 4686 });
+            var currentPointG = new esri.Graphic(currentPoint, markerG);
+            currentPointG.setAttributes({ conversacionId: currentChats[i].id });
+            glPointG.add(currentPointG, null, null);
+        } catch (err) {
+
+        }
+
+
+    }
+};
+
+function updateParticipantes() {
+    $("#listadoParticipantes").html("");
+    for (var i = 0; i < cacheParticipantes.length; i++) {
+        var strHtml = "<li>";
+        strHtml = strHtml + "<div class='item-content'>";
+        //strHtml = strHtml + "<div class='item-media'><img src='images/Profile.jpg' width='34' height='34'></div>";
+        strHtml = strHtml + "<div class='item-inner'>";
+        strHtml = strHtml + "<div class='item-title-row'>";
+        strHtml = strHtml + "<div class='item-title'>" + cacheParticipantes[i] + "</div>";
+        strHtml = strHtml + "<div class='item-subtitle'><a href='#' class=''><i class='fa fa-trash' aria-hidden='true'></i></a></div>";
+        strHtml = strHtml + "</div>";
+        strHtml = strHtml + "</div>";
+        strHtml = strHtml + "</div>";
+        strHtml = strHtml + "</li>";
+        $("#listadoParticipantes").append(strHtml);
+    };
 };
 
 function logout() {
@@ -373,7 +629,7 @@ function logout() {
                 gotoLogin();
             },
             function (msg) {
-                
+
             }
         );
     } else {
