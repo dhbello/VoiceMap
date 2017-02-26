95b3f0b02c559085e96c77c11b138cbce377065c
David Bello <david.bello@sinesat.com>
2017-02-25 19:58:47 -0500

    Version 0.25

---
+++ arcgis/dojo/cldr/nls/id/currency.js
@@ -0,0 +1,2 @@
+//>>built
+define("dojo/cldr/nls/id/currency",{AUD_displayName:"Dolar Australia",AUD_symbol:"AU$",CAD_displayName:"Dolar Kanada",CAD_symbol:"CA$",CHF_displayName:"Franc Swiss",CNY_displayName:"Yuan China",CNY_symbol:"CN\u00a5",EUR_displayName:"Euro",EUR_symbol:"\u20ac",GBP_displayName:"Pound Sterling Inggris",GBP_symbol:"\u00a3",HKD_displayName:"Dolar Hong Kong",HKD_symbol:"HK$",JPY_displayName:"Yen Jepang",JPY_symbol:"JP\u00a5",USD_displayName:"Dolar Amerika Serikat",USD_symbol:"US$"});
---
+++ arcgis/dojo/cldr/nls/id/number.js
@@ -0,0 +1,2 @@
+//>>built
+define("dojo/cldr/nls/id/number",{decimal:",",group:".",list:";",percentSign:"%",plusSign:"+",minusSign:"-",exponential:"E",superscriptingExponent:"\u00d7",perMille:"\u2030",infinity:"\u221e",nan:"NaN",decimalFormat:"#,##0.###","decimalFormat-long":"000 triliun","decimalFormat-short":"000\u00a0T",scientificFormat:"#E0",percentFormat:"#,##0%",currencyFormat:"\u00a4#,##0.00;(\u00a4#,##0.00)"});
